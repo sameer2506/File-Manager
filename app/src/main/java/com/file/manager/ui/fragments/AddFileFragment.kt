9fd88a9dd3ea31605f25f79a33cbe42e279e4758
Sameer <sameerkr679@gmail.com>
2022-01-26 09:21:12 +0530

    launch file

--- app/src/main/java/com/file/manager/ui/fragments/AddFileFragment.kt
+++ app/src/main/java/com/file/manager/ui/fragments/AddFileFragment.kt
@@ -2,12 +2,11 @@ package com.file.manager.ui.fragments
 
 import android.content.Context
 import android.os.Bundle
-import android.os.Environment
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.os.bundleOf
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -21,23 +20,28 @@ import com.file.manager.utils.launchFileIntent
 import com.file.manager.utils.log
 import java.io.File
 
-class FileListFragment : Fragment(), FileListRecyclerAdapter.OnItemClick {
+class AddFileFragment : Fragment(), FileListRecyclerAdapter.OnItemClick {
 
     private lateinit var fragmentContext: Context
     private lateinit var fragmentActivity: FragmentActivity
 
     private lateinit var binding: FragmentFileListBinding
 
-    private var fileList: ArrayList<FileModel> = ArrayList()
+    private var path: String = ""
+
+    private val fileList: ArrayList<FileModel> = ArrayList()
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
+    ): View? {
         fragmentContext = requireContext()
         fragmentActivity = requireActivity()
 
         binding = FragmentFileListBinding.inflate(layoutInflater)
+
+        path = requireArguments().getString("pathId")!!
 
         return binding.root
     }
@@ -45,21 +49,23 @@ class FileListFragment : Fragment(), FileListRecyclerAdapter.OnItemClick {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        listExternalStorage()
+        val file = File(path, File.separator)
+
+        listFiles(file)
 
         binding.fileRecyclerView.apply {
             layoutManager = LinearLayoutManager(fragmentContext)
             setHasFixedSize(true)
-            adapter = FileListRecyclerAdapter(fileList, this@FileListFragment)
+            adapter = FileListRecyclerAdapter(fileList, this@AddFileFragment)
         }
+
 
     }
 
     override fun onClick(fileModel: FileModel) {
-        if (fileModel.fileType == FileType.FOLDER){
+        if (fileModel.fileType == FileType.FOLDER) {
             addFileFragment(fileModel)
-        }
-        else{
+        } else {
             fragmentContext.launchFileIntent(fileModel)
         }
     }
@@ -77,20 +83,12 @@ class FileListFragment : Fragment(), FileListRecyclerAdapter.OnItemClick {
     }
 
 
-    private fun listExternalStorage(){
-        val state = Environment.getExternalStorageState()
-
-        if (Environment.MEDIA_MOUNTED == state || Environment.MEDIA_MOUNTED_READ_ONLY == state){
-            listFiles(Environment.getExternalStorageDirectory())
-        }
-    }
-
-    private fun listFiles(directory: File){
+    private fun listFiles(directory: File) {
         val files = directory.listFiles()
-        if (files != null){
-            for (file in files){
-                if (file != null){
-                    if (file.isDirectory){
+        if (files != null) {
+            for (file in files) {
+                if (file != null) {
+                    if (file.isDirectory) {
                         val data = FileModel(
                             file.path,
                             FileType.getFileType(file),
@@ -102,8 +100,7 @@ class FileListFragment : Fragment(), FileListRecyclerAdapter.OnItemClick {
                             R.drawable.folder_list_icon.toString()
                         )
                         fileList.add(data)
-                    }
-                    else{
+                    } else {
                         val data = FileModel(
                             file.path,
                             FileType.getFileType(file),
@@ -120,6 +117,5 @@ class FileListFragment : Fragment(), FileListRecyclerAdapter.OnItemClick {
             }
         }
     }
-
 
 }
